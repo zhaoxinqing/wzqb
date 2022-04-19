1776a571e173114498ed5ff708b3a2829f91bf15
ZhaoXinqing <1375565592@qq.com>
2022-04-19 17:07:11 +0800

    feat：json tool.

---
+++ json.go
@@ -0,0 +1,28 @@
+package newcelebration
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Convert json string to map
+func JsonToMap(jsonStr string) (map[string]string, error) {
+	m := make(map[string]string)
+	err := json.Unmarshal([]byte(jsonStr), &m)
+	if err != nil {
+		fmt.Printf("Unmarshal with error: %+v\n", err)
+		return nil, err
+	}
+	return m, nil
+}
+
+// Convert map json string
+func MapToJson(m map[string]string) (string, error) {
+	jsonByte, err := json.Marshal(m)
+	if err != nil {
+		fmt.Printf("Marshal with error: %+v\n", err)
+		return "", nil
+	}
+
+	return string(jsonByte), nil
+}
